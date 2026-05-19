2faf177957e8d7be04288b3e3e2a1ea807286625
Mehedi Hasan <mehedihasanhridoy20@gmail.com>
2026-05-19 14:44:01 +0300

    action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Tells Next.js to compile to static HTML
+  images: {
+    unoptimized: true, // Required for static exports using next/image
+  },
 };
 
 export default nextConfig;
